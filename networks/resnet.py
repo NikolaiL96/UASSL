01400e261a9d4494ac419b2d821d82a6532d4d85
nikolai.lorenz <lorenz-nikolai@web.de>
2024-01-22 12:45:25 +0100

    Added ResNet50 ImageNet wheights (supervised) for error detection.

--- networks/resnet.py
+++ networks/resnet.py
@@ -1,5 +1,5 @@
 import torch.nn as nn
-from torchvision.models import resnet18, resnet50
+from torchvision.models import resnet18, resnet50, ResNet50_Weights
 
 
 def cifar_resnet18(in_channels: int = 3):
@@ -13,7 +13,8 @@ def cifar_resnet18(in_channels: int = 3):
 
 
 def cifar_resnet50(in_channels: int = 3):
-    resnet = resnet50(zero_init_residual=True)
+    #resnet = resnet50(zero_init_residual=True)
+    resnet = resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
 
     # Cifar specifics
     resnet.conv1 = nn.Conv2d(in_channels, 64, 3, 1, 1, bias=False)
