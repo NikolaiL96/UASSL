f9fc2cd4e5c32cd323c4bfe3947ff4d2130e6267
nikolai.lorenz <lorenz-nikolai@web.de>
2024-01-22 12:45:58 +0100

    Removed ResNet50 ImageNet wheights (supervised).

--- networks/resnet.py
+++ networks/resnet.py
@@ -13,8 +13,8 @@ def cifar_resnet18(in_channels: int = 3):
 
 
 def cifar_resnet50(in_channels: int = 3):
-    #resnet = resnet50(zero_init_residual=True)
-    resnet = resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
+    resnet = resnet50(zero_init_residual=True)
+    #resnet = resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
 
     # Cifar specifics
     resnet.conv1 = nn.Conv2d(in_channels, 64, 3, 1, 1, bias=False)
