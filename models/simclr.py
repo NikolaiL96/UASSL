c1f872c6dbd063ee5e3dfbd45f4f1370399bf984
nikolai.lorenz <lorenz-nikolai@web.de>
2024-01-22 10:26:12 +0100

    Bug fixes

--- models/simclr.py
+++ models/simclr.py
@@ -84,11 +84,8 @@ class SimCLR(nn.Module):
     def compute_ssl_loss(self, dist1, dist2):
         n_batch = dist1.loc.shape[0]
         if self.loss == "NT-Xent":
-            a = dist1.mean
-            b = dist1.loc
-            c = dist1.rsample()
-            p1 = self.projector(dist1.mean)
-            p2 = self.projector(dist2.mean)
+            p1 = self.projector(dist1.rsample())
+            p2 = self.projector(dist2.rsample())
             ssl_loss = self.loss_fn(p1, p2)
 
         elif "MCNT-Xent" in self.loss:
