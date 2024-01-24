3645d28c817d07728e87ce5eb8297daa39f5355b
nikolai.lorenz <lorenz-nikolai@web.de>
2024-01-24 12:42:42 +0100

    Out-commented grad_clip_norm

--- trainer/SSL_trainer.py
+++ trainer/SSL_trainer.py
@@ -123,7 +123,7 @@ class SSL_Trainer(object):
             self.scaler.unscale_(self.optimizer)
 
             # Prevent exploding kappa by clipping gradients
-            torch.nn.utils.clip_grad_norm_(self.model.backbone_net.parameters(), self.clip)
+            #torch.nn.utils.clip_grad_norm_(self.model.backbone_net.parameters(), self.clip)
 
             self.scaler.step(self.optimizer)
             self.scaler.update()
